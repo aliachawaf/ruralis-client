bc462ab0dc3c4182a4200ad6a0ca2b2d6dc18ed4
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2020-01-06 16:18:03 +0100

    ADD - Header to game rules page

--- src/components/GameRules/GameRules.js
+++ src/components/GameRules/GameRules.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { PDFReader } from 'reactjs-pdf-reader'
 import rules from '../../assets/gameRules.pdf'
+import RuralisHeader from '../common/RuralisHeader'
 
 const GameRules = () => (
-
-  <PDFReader
-    url={rules}
-    showAllPage
-    scale={1.5}
-  />
+  <div>
+    <RuralisHeader title='Règles du jeu' />
+    <PDFReader
+      url={rules}
+      showAllPage
+      scale={1.5}
+    />
+  </div>
 )
 
 export default GameRules

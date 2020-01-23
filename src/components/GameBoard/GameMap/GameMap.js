68c4311dfee2462c30dc2d38fa901ec338ca27fa
Alia Chawaf <alia.chawaf@etu.umontpellier.fr>
2020-01-23 09:59:56 +0100

    Remove edit button from map

--- src/components/GameBoard/GameMap/GameMap.js
+++ src/components/GameBoard/GameMap/GameMap.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+import 'leaflet-polylinedecorator'
 import L from 'leaflet'
 import { FeatureGroup, ImageOverlay, Map, ScaleControl } from 'react-leaflet'
 import { EditControl } from 'react-leaflet-draw'
@@ -77,6 +79,9 @@ const GameMap = React.forwardRef((props, ref) => {
             polygon: (iaeSelectedDrawingType === 'polygon') ? { showLength: true } : false,
             rectangle: (iaeSelectedDrawingType === 'polygon'),
             circlemarker: (iaeSelectedDrawingType === 'circlemarker')
+          }}
+          edit={{
+            edit: false
           }}
         />
       </FeatureGroup>
